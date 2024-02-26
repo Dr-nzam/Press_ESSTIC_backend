ad08b1c91af2a7a5f4f652bfc8c3208d6277cbc1
unknown <nzambouarmel.com>
2024-02-26 08:24:26 +0100

    ïnstalation creation du projet

---
+++ Press_ESSTIC/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Press_ESSTIC project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Press_ESSTIC.settings')
+
+application = get_wsgi_application()
