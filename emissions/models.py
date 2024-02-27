8ff69be5d4ec0a6950eb8c890dd2ba800b415078
unknown <nzambouarmel.com>
2024-02-27 09:54:07 +0100

    emission debut

--- emissions/models.py
+++ emissions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Emissions(models.Model):
+    nom = models.CharField(max_length = 128)
+    image = models.FileField(upload_to='assets/imagesemissions/',
+                             default='',blank=True, max_length=250)
+    video = models.FileField(upload_to='assets/videoemissions/',
+                             default='',blank=True, max_length=250)
+    audio =models.FileField(upload_to='assets/audioemissions/',
+                             default='',blank=True, max_length=250)
+    
+    def __str__(self) :
+        return self.nom
